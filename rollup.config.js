a74c98f9056e29ae51a40e17f03cb8487a0bf893
vkuzmenko <vkuzmenko@uwm.com>
2025-11-27 15:00:27 -0500

    Test

--- rollup.config.js
+++ rollup.config.js
@@ -24,6 +24,7 @@ export default {
   external: ['react', 'react-dom', 'decky-frontend-lib'],
   output: {
     file: 'dist/index.js',
+    name: 'PluginHANotify',
     globals: {
       react: 'SP_REACT',
       'react-dom': 'SP_REACTDOM',
